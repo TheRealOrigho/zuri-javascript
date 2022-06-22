1bce5479ed6b7bf36a96e9ad1587dea0425c72b6
Kevin Origho <orighokevin@gmail.com>
2022-06-22 09:02:07 +0100

    zuri first javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+let myName = 'My name is Kevin Origho \n'
+let height = 'I am 5"7 \n'
+let country = 'my country is Nigeria'
+
+alert(myName  + height + country)
+
+console.log(myName);
+console.log(height);
+console.log(country);
